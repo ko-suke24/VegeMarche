6a16034f531a3a651b6e0878a47f78e2017d8251
ko-suke24 <kosuke0322@gmail.com>
2021-01-12 01:12:32 +0900

    email一意性強制

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_154930) do
+ActiveRecord::Schema.define(version: 2021_01_11_155413) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
